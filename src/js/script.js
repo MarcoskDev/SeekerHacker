4d32e84b045318b1e502f63033f6011a337a2970
ShadeHacker <154840423+MarcoskDev@users.noreply.github.com>
2025-02-16 18:29:28 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('contextmenu', (event) => event.preventDefault());
+
+/* const customAlert = document.getElementById('alert');
+
+// Função para exibir a mensagem
+function showCustomAlert() {
+  customAlert.style.display = 'block'; // Mostra o alerta
+  setTimeout(() => {
+    customAlert.style.display = 'none'; // Oculta o alerta após 2 segundos
+  }, 2000);
+}
+
+// Bloqueio de atalhos
+document.addEventListener('keydown', (event) => {
+  if (
+    event.ctrlKey ||
+    event.shiftKey ||
+    event.altKey ||
+    event.key.length === 1
+  ) {
+    event.preventDefault(); // Impede o comportamento padrão
+    event.stopPropagation(); // Para a propagação do evento
+    showCustomAlert(); // Mostra a mensagem
+  }
+});
+ */
